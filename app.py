c8e10a586dffb8d275abcc3c2434d4a6b1ed11dc
yatogg <valerapolatinskij203@gmail.com>
2026-05-18 13:38:49 +0900

    fix sqlite path

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yakutsk_cold_dev_secret_2024'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
